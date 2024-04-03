ff4ef7bc4a50e4e97258194331361b85814bacea
TommasoSviluppatore <tubia.st.tommaso@maxplanck.edu.it>
2024-04-03 08:41:16 +0200

    sfsfd
    dfsfsdsdfsf

--- src/client/Avviatore.java
+++ src/client/Avviatore.java
@@ -11,9 +11,7 @@ import javax.xml.*;
 import javax.xml.bind.*;
 import javax.xml.bind.annotation.*;
 
-import control.Controller_Configurazione;
-import view.Finestra_Borsa;
-import view.Finestra_Configurazione;
+
 import view.Finestra_MenuPrincipale;
 import control.Controller_DatiApptxt;
 
@@ -25,7 +23,7 @@ public class Avviatore {
 
 		/**acaso*/
 		Finestra_MenuPrincipale schermataPrincipale=new Finestra_MenuPrincipale();
-		Finestra_Borsa schermataBorsa = new Finestra_Borsa();
+		schermataPrincipale.setVisible(true);
 		
 		
 		EventQueue.invokeLater(new Runnable() {
@@ -34,10 +32,8 @@ public class Avviatore {
 				try {
 				
 					/**avvio instanza in sottofondo passandogli il controller*/
-					Finestra_Configurazione schermataConfigurazione= new Finestra_Configurazione();
-					schermataConfigurazione.setVisible(true);
+					
 					Controller_DatiApptxt c1=new Controller_DatiApptxt();
-					Controller_Configurazione c=new Controller_Configurazione(schermataConfigurazione, c1);
 					
 					
 				}catch(Exception e) {
